64da48c329afc9876714d9ec3f070f6cdfa394ec
sulton <global-sultonmalik326@gmail.com>
2024-01-10 02:01:35 +0500

    Add project files.

---
+++ ConsoleApp30/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+
+namespace ConsoleApp30
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            bool isCorrected = false;
+            Random rand = new Random();
+
+            int randomNum = rand.Next(1,11);
+
+            Console.WriteLine("Welcome to the number guessing game...");
+            Console.WriteLine("A number between 1 and 10 will be generated");
+            Console.WriteLine("If you guess the correct number , you win!");
+
+
+           
+
+            while (!isCorrected)
+            {
+                Console.WriteLine("Please enter your guess...");
+                int guess = Convert.ToInt32(Console.ReadLine());
+
+                if(guess > randomNum)
+                {
+                    Console.WriteLine("Your guess is to high");
+                }else if(guess < randomNum)
+                {
+                    Console.WriteLine("Your guess is to low");
+                }else {
+
+                    Console.WriteLine("Correct!");
+                    isCorrected = true;
+                        
+                     }
+
+
+            }
+            Console.WriteLine("Conguratulation");
+
+            Console.ReadKey();
+
+        }
+    }
+}
